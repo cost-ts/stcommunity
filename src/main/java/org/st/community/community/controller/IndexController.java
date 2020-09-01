bca54d56c23fadc5a110327bba4de4a05351fa45
st <shutteng@163.com>
2020-09-02 00:07:42 +0800

    introduce Boorstrap and create index

---
+++ src/main/java/org/st/community/community/controller/IndexController.java
@@ -0,0 +1,11 @@
+package org.st.community.community.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: ST
+ * Date: 2020-09-01
+ * Time: 23:52
+ */
+public class IndexController {
+}
